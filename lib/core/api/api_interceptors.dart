9af2cf7c70ce15dc961b052158f465af4163490f
pavlysamir <bavlysamir33@gmail.com>
2025-05-23 02:12:25 +0300

    Add paginated list view and cubit implementation; update API interceptor for navigation and authentication

--- lib/core/api/api_interceptors.dart
+++ lib/core/api/api_interceptors.dart
@@ -1,15 +1,32 @@
 import 'package:dio/dio.dart';
+import 'package:ithera_app/core/cashe/cache_helper.dart';
+import 'package:ithera_app/core/cashe/cashe_constance.dart';
 import 'dart:convert';
+
+import 'package:ithera_app/core/routing/navigation_services.dart';
+import 'package:ithera_app/core/routing/routes.dart';
 
 class ApiInterceptor extends Interceptor {
   ApiInterceptor();
   @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
-    const String username = '11236180'; // Replace with your username
-    const String password = '60-dayfreetrial'; // Replace with your password
+  void onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
+    // final token =
+    //     await CacheHelper.getSecureData(key: CacheConstants.token) ?? '';
+    // if (token != '') {
+    //   options.headers['Authorization'] = 'Bearer $token';
+    // } else {
+    //   const String username = '11236180';
+    //   const String password = '60-dayfreetrial';
+    //   final String basicAuth =
+    //       'Basic ${base64Encode(utf8.encode('$username:$password'))}';
+    //   options.headers['Authorization'] = basicAuth;
+    // }
+
+    const String username = '11236180';
+    const String password = '60-dayfreetrial';
     final String basicAuth =
         'Basic ${base64Encode(utf8.encode('$username:$password'))}';
-
     options.headers['Authorization'] = basicAuth;
     options.headers['Content-Type'] = 'application/json';
     super.onRequest(options, handler);
@@ -25,9 +42,7 @@ class ApiInterceptor extends Interceptor {
               .contains('invalid_token')) {
         // Token is expired, navigate to login screen
         // navigatorKey.currentState?.pushReplacementNamed();
-        // customGoAndDeleteNavigate(
-        //     context: navigatorKey.currentState!.context,
-        //     path: AppRouter.kLoginScreen);
+        NavigationService().navigateToReplacement(Routes.welcomeScreen);
       }
     }
     super.onError(err, handler);
