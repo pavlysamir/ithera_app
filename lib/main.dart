dcf8e0d460c4a894b94c6af01f8a8d3aa3e044fe
pavlysamir <bavlysamir33@gmail.com>
2025-05-29 22:30:27 +0300

    Integrate Firebase into the iThera app, update package names, and configure necessary files

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ithera_app/core/cashe/cache_helper.dart';
 import 'package:ithera_app/core/di/service_locator.dart';
 import 'package:ithera_app/core/helpers/simple_bloc_observer.dart';
+import 'package:ithera_app/firebase_options.dart';
 import 'package:ithera_app/iThera_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await CacheHelper.init();
   setUpServiceLocator();
   Bloc.observer = SimpleBlocObserver();
