6c74bbb16a86a6fd92a606ea1790e996839575a4
pavlysamir <bavlysamir33@gmail.com>
2025-05-30 20:50:31 +0300

    Refactor PatientBookingCubit to include repository dependency injection and prepare for future booking retrieval methods

--- lib/features/booking/patient_booking/managers/cubit/patient_booking_cubit.dart
+++ lib/features/booking/patient_booking/managers/cubit/patient_booking_cubit.dart
@@ -1,8 +1,28 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:ithera_app/features/booking/patient_booking/data/repos/patient_booking_repo.dart';
 
 part 'patient_booking_state.dart';
 
 class PatientBookingCubit extends Cubit<PatientBookingState> {
-  PatientBookingCubit() : super(PatientBookingInitial());
+  PatientBookingCubit(this._patientBookingRepo)
+      : super(PatientBookingInitial());
+  final PatientBookingRepo _patientBookingRepo;
+
+  // Future<void> getPatientBookings({required int status}) async {
+  //   emit(PatientBookingLoading());
+  //   final result = await _patientBookingRepo.getPatientBookings(status: status);
+  //   result.fold(
+  //     (error) => emit(PatientBookingError(error)),
+  //     (bookings) => emit(PatientBookingLoaded(bookings)),
+  //   );
+  // }
+  // Future<void> refreshBookings({required int status}) async {
+  //   emit(PatientBookingLoading());
+  //   final result = await _patientBookingRepo.getPatientBookings(status: status);
+  //   result.fold(
+  //     (error) => emit(PatientBookingError(error)),
+  //     (bookings) => emit(PatientBookingRefreshed(bookings)),
+  //   );
+  // }
 }
