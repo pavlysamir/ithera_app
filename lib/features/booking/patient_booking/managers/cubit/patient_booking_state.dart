e2d5de6f795b920d5abdbbc6e13152e0eb8c24fe
pavlysamir <bavlysamir33@gmail.com>
2025-05-31 19:04:03 +0300

    feat: Implement patient booking management with cubit architecture
    - Added PatientBookingCubit to manage patient booking states and actions.
    - Integrated patient booking initialization and refresh functionality.
    - Enhanced patient booking screen to display active and last bookings.
    - Created custom widgets for displaying active and last patient bookings.
    - Updated service locator to include PatientBookingRepo and PatientBookingCubit.
    - Refactored home layout to include patient booking functionality.
    - Improved error handling and loading states in patient booking management.
    - Removed unused dependencies from pubspec.yaml and pubspec.lock.

--- lib/features/booking/patient_booking/managers/cubit/patient_booking_state.dart
+++ lib/features/booking/patient_booking/managers/cubit/patient_booking_state.dart
@@ -1,10 +1,42 @@
 part of 'patient_booking_cubit.dart';
 
-sealed class PatientBookingState extends Equatable {
+abstract class PatientBookingState extends Equatable {
   const PatientBookingState();
 
   @override
   List<Object> get props => [];
 }
 
-final class PatientBookingInitial extends PatientBookingState {}
+class PatientBookingInitial extends PatientBookingState {}
+
+class PatientBookingLoading extends PatientBookingState {
+  final List<PatientBookingModel> activeBookings;
+  final List<PatientBookingModel> lastBookings;
+
+  const PatientBookingLoading(this.activeBookings, this.lastBookings);
+
+  @override
+  List<Object> get props => [activeBookings, lastBookings];
+}
+
+class PatientBookingLoaded extends PatientBookingState {
+  final List<PatientBookingModel> activeBookings;
+  final List<PatientBookingModel> lastBookings;
+
+  const PatientBookingLoaded(this.activeBookings, this.lastBookings);
+
+  @override
+  List<Object> get props => [activeBookings, lastBookings];
+}
+
+class PatientBookingError extends PatientBookingState {
+  final String message;
+  final List<PatientBookingModel> activeBookings;
+  final List<PatientBookingModel> lastBookings;
+
+  const PatientBookingError(
+      this.message, this.activeBookings, this.lastBookings);
+
+  @override
+  List<Object> get props => [message, activeBookings, lastBookings];
+}
--- lib/features/booking/patient_booking/presentation/screens/no_booking_screen.dart
+++ lib/features/booking/patient_booking/presentation/screens/no_booking_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ithera_app/core/assets/assets.dart';
+import 'package:ithera_app/core/routing/navigation_services.dart';
+import 'package:ithera_app/core/routing/routes.dart';
 import 'package:ithera_app/core/theme/app_colors.dart';
 import 'package:ithera_app/core/theme/app_text_styles.dart';
 import 'package:ithera_app/core/widgets/custom_button_large.dart';
@@ -45,7 +47,10 @@ class NoBookingScreen extends StatelessWidget {
           CustomButtonLarge(
               text: 'الصفحة الرئيسية',
               color: AppColors.primaryColor,
-              function: () {}),
+              function: () {
+                NavigationService()
+                    .navigateToReplacement(Routes.patientHomeLayout);
+              }),
         ],
       ),
     );
